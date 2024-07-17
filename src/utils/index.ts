e5203476e0492f88a343830a8b46bf2074dba4fd
Mehmet Berat Öztürk <77115599+BeratOz01@users.noreply.github.com>
2024-07-17 13:53:30 +0300

    feat: poseidon encryption & decryption implemented
    * test: poseidon test are written

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ export const SNARK_FIELD_SIZE =
   21888242871839275222246405745257275088548364400416034343698204186575808495617n;
 
 // parses the object to bigint object
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export function unstringifyBigInts(o: any): any {
   if (typeof o === "string") {
     if (/^[0-9]+$/.test(o) || /^0x[0-9a-fA-F]+$/.test(o)) {
