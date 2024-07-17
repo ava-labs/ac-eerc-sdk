da3de5d4e387ee21d9f0b6f5b6d4ee354b1e9d9d
Mehmet Berat Öztürk <77115599+BeratOz01@users.noreply.github.com>
2024-07-17 18:54:47 +0300

    feat: register proof generation implemented
    * feat: key derivation implemented
    * feat: proof generator class

--- src/EERC.ts
+++ src/EERC.ts
@@ -1,9 +1,18 @@
 import type { PublicClient, WalletClient } from "wagmi";
-import { ERC34_ABI, MESSAGES } from "./utils";
+import { BabyJub } from "./crypto/babyjub";
+import { FF } from "./crypto/ff";
+import { formatKeyForCurve, getPrivateKeyFromSignature } from "./crypto/key";
+import { ProofGenerator } from "./helpers";
+import type { IProof } from "./helpers/types";
+import { ERC34_ABI, MESSAGES, SNARK_FIELD_SIZE } from "./utils";
 
 export class EERC {
   private client: PublicClient;
   private wallet: WalletClient;
+  public curve: BabyJub;
+  public field: FF;
+  public proofGenerator: ProofGenerator;
+
   public contractAddress: `0x${string}`;
   public abi = ERC34_ABI;
 
@@ -15,6 +24,10 @@ export class EERC {
     this.client = client;
     this.wallet = wallet;
     this.contractAddress = contractAddress;
+
+    this.field = new FF(SNARK_FIELD_SIZE);
+    this.curve = new BabyJub(this.field);
+    this.proofGenerator = new ProofGenerator();
   }
 
   async fetchContractData() {
@@ -40,7 +53,10 @@ export class EERC {
   }
 
   // function to register a new user to the contract
-  async register() {
+  async register(
+    wasmPath: string,
+    zkeyPath: string,
+  ): Promise<{ key: string; error: string; proof: IProof | null }> {
     if (!this.wallet || !this.client)
       throw new Error("Wallet or client not provided");
 
@@ -51,30 +67,26 @@ export class EERC {
         this.contractAddress,
       );
 
-      const signature = await this.wallet.signTypedData({
-        domain: {
-          name: "AvaCloud",
-          version: "1",
-          chainId: this.client.chain.id,
-          verifyingContract: this.contractAddress,
-        },
-        types: {
-          User: [
-            { name: "address", type: "address" },
-            { name: "contractAddress", type: "address" },
-            { name: "content", type: "string" },
-          ],
-        },
-        primaryType: "User",
-        message: {
-          address: this.wallet.account.address,
-          contractAddress: this.contractAddress,
-          content: message,
-        },
-      });
-      return signature;
+      const signature = await this.wallet.signMessage({ message });
+      const key = getPrivateKeyFromSignature(signature);
+      const formatted = formatKeyForCurve(key);
+      const publicKey = this.curve.generatePublicKey(formatted);
+
+      const input = {
+        sk: String(formatted),
+        pk: publicKey.map(String),
+      };
+
+      const proof = await this.proofGenerator.generateRegisterProof(
+        input,
+        wasmPath,
+        zkeyPath,
+      );
+
+      // returns proof for the transaction
+      return { key, error: "", proof };
     } catch (e) {
-      return e;
+      return { key: "", error: "error on registration", proof: null };
     }
   }
 }
