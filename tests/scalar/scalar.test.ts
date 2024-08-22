4f9eea3abd620cff4f643bf8b4e214ace76c21c4
Mehmet Berat Öztürk <77115599+BeratOz01@users.noreply.github.com>
2024-08-22 16:44:58 +0300

    test: bsgs test added

--- tests/scalar/scalar.test.ts
+++ tests/scalar/scalar.test.ts
@@ -1,5 +1,5 @@
 import { Scalar } from "../../src/crypto/scalar";
-import { shiftRightTestCases } from "./scalar.test.cases";
+import { decideCases, shiftRightTestCases } from "./scalar.test.cases";
 
 describe("Scalar", () => {
   test("shiftRight should handle properly", () => {
@@ -63,82 +63,7 @@ describe("Scalar", () => {
   });
 
   test("decide should handle properly", () => {
-    const cases = [
-      {
-        input: {
-          oldWhole: 100n,
-          oldFractional: 50n,
-          amountWhole: 20n,
-          amountFractional: 30n,
-        },
-        expected: {
-          toBeSubtracted: [20n, 30n],
-          toBeAdded: [0n, 0n],
-        },
-      },
-      {
-        input: {
-          oldWhole: 50n,
-          oldFractional: 25n,
-          amountWhole: 50n,
-          amountFractional: 25n,
-        },
-        expected: {
-          toBeSubtracted: [50n, 25n],
-          toBeAdded: [0n, 0n],
-        },
-      },
-      {
-        input: {
-          oldWhole: 30n,
-          oldFractional: 75n,
-          amountWhole: 10n,
-          amountFractional: 50n,
-        },
-        expected: {
-          toBeSubtracted: [10n, 50n],
-          toBeAdded: [0n, 0n],
-        },
-      },
-      {
-        input: {
-          oldWhole: 15n,
-          oldFractional: 50n,
-          amountWhole: 15n,
-          amountFractional: 25n,
-        },
-        expected: {
-          toBeSubtracted: [15n, 25n],
-          toBeAdded: [0n, 0n],
-        },
-      },
-      {
-        input: {
-          oldWhole: 20n,
-          oldFractional: 10n,
-          amountWhole: 19n,
-          amountFractional: 20n,
-        },
-        expected: {
-          toBeSubtracted: [20n, 0n],
-          toBeAdded: [0n, 80n],
-        },
-      },
-      {
-        input: {
-          oldWhole: 20n,
-          oldFractional: 10n,
-          amountWhole: 19n,
-          amountFractional: 90n,
-        },
-        expected: {
-          toBeSubtracted: [20n, 0n],
-          toBeAdded: [0n, 10n],
-        },
-      },
-    ];
-
-    for (const c of cases) {
+    for (const c of decideCases) {
       expect(
         Scalar.decide(
           c.input.oldWhole,
