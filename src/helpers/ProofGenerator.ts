29570a9d99beae981dfaa50538bc6117119bbe73
mehmet.ozturk <mehmet.ozturk@avalabs.org>
2024-09-03 12:23:07 +0300

    chore: snarkjs removed completely

--- src/helpers/ProofGenerator.ts
+++ src/helpers/ProofGenerator.ts
@@ -1,4 +1,3 @@
-import { groth16 } from "snarkjs";
 import { logMessage } from "./logger";
 import type { IProof, ProofType } from "./types";
 
@@ -16,24 +15,10 @@ export class ProofGenerator {
     const { wasm, zkey } = this.getPaths(type);
 
     logMessage(`Generating proof for ${type}...`);
-    const { proof, publicSignals } = await groth16.fullProve(input, wasm, zkey);
-    logMessage(`Proof generated for ${type}`);
-    const calldata = await groth16.exportSolidityCallData(proof, publicSignals);
-    return this.convertCallData(calldata);
-  }
-
-  // converts snarkjs generated calldata to a solidity input object
-  private convertCallData(calldata: string): IProof {
-    const argv = calldata.replace(/["[\]\s]/g, "").split(",");
-
-    const a = [argv[0], argv[1]] as [string, string];
-    const b = [
-      [argv[2], argv[3]],
-      [argv[4], argv[5]],
-    ] as [string[], string[]];
-    const c = [argv[6], argv[7]] as [string, string];
-    const input = argv.slice(8, argv.length);
-
-    return { a, b, c, input };
+    // const { proof, publicSignals } = await groth16.fullProve(input, wasm, zkey);
+    // logMessage(`Proof generated for ${type}`);
+    // const calldata = await groth16.exportSolidityCallData(proof, publicSignals);
+    // return this.convertCallData(calldata);
+    return {} as IProof;
   }
 }
